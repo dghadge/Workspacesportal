388b41ca8d2a49a6dfe52aaf3548b84481e1ffc1
naumfujimori <39249830+naumfujimori@users.noreply.github.com>
2018-05-14 11:36:22 +1000

    Update config.js

--- website/js/config.js
+++ website/js/config.js
@@ -1,13 +1,13 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_IRw4g9Wrq', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '107sjdu70fqpki8mda20clu4ka', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'ap-southeast-2_uLhSiSBXP', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7oeceh0ejqlss5umbctj81jt16', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://gjuy958258.execute-api.ap-southeast-2.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     },
     approval: {
-        email: 'earl@eeg3.net'
+        email: 'naum.fujimori@cmdsolutions.com.au'
     }
 };
