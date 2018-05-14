95deb200253747b8f4a8b2cd1a13e66c1cdcc631
naumfujimori <39249830+naumfujimori@users.noreply.github.com>
2018-05-14 14:06:11 +1000

    Update config.js

--- website/js/config.js
+++ website/js/config.js
@@ -1,11 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-southeast-2_uLhSiSBXP', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7oeceh0ejqlss5umbctj81jt16', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-southeast-2_6suMb3gDh', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '40smnvsd4as4ig4546qsr5fajj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://gjuy958258.execute-api.ap-southeast-2.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: ' https://e0gfpi74lh.execute-api.ap-southeast-2.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     },
     approval: {
         email: 'naum.fujimori@cmdsolutions.com.au'
