14742de59afe11437c89a1a51768001b926b9553
naumfujimori <39249830+naumfujimori@users.noreply.github.com>
2018-05-16 09:38:15 +1000

    Update workspaces-control.js

--- lambda/workspaces-control/workspaces-control.js
+++ lambda/workspaces-control/workspaces-control.js
@@ -17,8 +17,8 @@ var lambda = new AWS.Lambda();
 exports.handler = (event, context, callback) => {
 
     var originURL = process.env.ORIGIN_URL || '*'; // Origin URL to allow for CORS
-    var stateMachine = process.env.STATE_MACHINE_ARN || 'arn:aws:states:us-east-1:375301133253:stateMachine:PromotionApproval'; // State Machine for 'create' action.
-    var detailsLambda = process.env.DETAILS_LAMBDA || 'wsp-db-int-serverless-stack-workspacesDetails-1J4ZB3URZF2QP';
+    var stateMachine = process.env.STATE_MACHINE_ARN || 'arn:aws:states:ap-southeast-2:240696153881:stateMachine:WSPortal-StateMachine'; // State Machine for 'create' action.
+    var detailsLambda = process.env.DETAILS_LAMBDA || 'wsportal-serverless-stack-workspacesDetails-ZTNJ1D599XAC';
 
     console.log('Received event:', JSON.stringify(event, null, 2)); // Output log for debugging purposes.
 
