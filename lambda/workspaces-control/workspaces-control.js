07ed6a4d390f3792731f2a0b5d6073cba409cdd8
Naum <naum.fujimori@cmdsolutions.com.au>
2018-06-18 10:27:22 +1000

    ok

--- lambda/workspaces-control/workspaces-control.js
+++ lambda/workspaces-control/workspaces-control.js
@@ -370,7 +370,7 @@ exports.handler = (event, context, callback) => {
         // Get the Amazon-owned bundle list first, and then get the Customer-owned bundles next, and then return the entire list.
         getBundles({
         
-            Owner: null
+            Owner: 'AMAZON'
         }, function () {
             getBundles({
                 Owner: null
